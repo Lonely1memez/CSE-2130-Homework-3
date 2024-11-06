c20eca6d744fe08e1f390613e0a4f55f3bc2707b
Lonely.memez <92387289+Lonely1memez@users.noreply.github.com>
2024-11-06 09:36:32 -0800

    Add files via upload
    These are labs 1 through 4. I'll go into detail on what each lab does.
    Lab 1: The numbers X and Y are found at locations x3100 and x3101
    Lab 2: The numbers X and Y are found at locations x3120 and x3121
    Lab 3: Write a program in LC-3 assembly language that keeps prompting for an integer in the range 0-6, and each time it outputs the corresponding name of the day. If a key other than ’0’ through ’6’ is pressed, the program exits.
    Lab 4: Write a program in LC-3 assembly language that computes Fn, the n−th Fibonacci number. Find the largest Fn such that no overflow occurs, i.e. find n = N such that FN is the largest Fibonacci number to be correctly represented with 16 bits in two’s complement format.

---
+++ Lab 3 Days.asm
@@ -0,0 +1,42 @@
+.ORIG x3000
+	LEA R0, PROMPT		; Prompt user for input
+	PUTS
+	GETC			; Get input from user
+
+; Convert input from ascii to integer
+	ADD R0, R0, #-16
+	ADD R0, R0, #-16
+	ADD R0, R0, #-16
+
+	ADD R0, R0, #0
+	BRn INVALID
+	ADD R0, R0, #-6
+	BRp INVALID
+;	OUT 
+
+
+LOOP				; Go through the days of the week
+	LEA R0, DAYS
+	ADD R0, R0, #0
+	BRz DISPLAY		; Day of week found, so display 	
+	ADD R0, R0, #10		; Go to the next day of the week 
+	ADD R0, R0, #-1		; Decrement the counter
+	BRp LOOP
+DISPLAY				; Display the day of the week
+	PUTS
+
+INVALID				; Input is invalid
+
+HALT
+
+PROMPT .STRINGZ "Please enter a number fR0m 0-6: "
+DAYS .STRINGZ "Sunday   "
+ .STRINGZ "Monday   "
+ .STRINGZ "Tuesday  "
+ .STRINGZ "Wendsday "
+ .STRINGZ "Thursday "
+ .STRINGZ "Friday   "
+ .STRINGZ "Saturday " 
+
+
+.END
